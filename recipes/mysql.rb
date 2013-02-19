ced66ce71b656eb1f583505498e358a973fcc1ce
Gabriel Mazetto <brodock@gmail.com>
2013-02-18 21:07:01 -0300

    Fixed a lot of things

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,8 +1,1 @@
-node.default['mumble']['mysql']['database'] = 'mumble'
-node.default['mumble']['mysql']['username'] = "mumble"
-node.default['mumble']['mysql']['password'] = SecureRandom.base64(32).gsub(/\W/, "")
-node.default['mumble']['mysql']['host'] = 'localhost'
-node.default['mumble']['mysql']['port'] = '3306'
-node.default['mumble']['mysql']['prefix'] = ''
-
 include_recipe "mumble::default"
