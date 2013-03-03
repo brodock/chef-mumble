b2ece543f97de022a88b248005545f16c8edbca4
cn <chrnicolai@gmail.com>
2013-03-03 21:00:25 +0100

    Fix wrong behavior when using mumble::default without mysql
    The changes in ced66ce71b656eb1f583505498e358a973fcc1ce made it impossible to setup murmur without mysql because attributes/mysql.rb is always executed by (at least my version [10.16] of) Chef and not only when a recipe with matching name is run. This seems to be a common misunderstanding of Chef attribute handling although I'm not able to find a reference in the public Chef doc - at least in my tests this holds true.
    
    (Even when setting the node['mumble']['mysql'] property to nil there is an error because set_unless['mumble']['mysql']['password'] has then trouble figuring out the if there is already a value set...)

--- attributes/mysql.rb
+++
@@ -1,6 +0,0 @@
-default['mumble']['mysql']['database'] = 'mumble'
-default['mumble']['mysql']['username'] = "mumble"
-set_unless['mumble']['mysql']['password'] = SecureRandom.base64(32).gsub(/\W/, "")
-default['mumble']['mysql']['host'] = 'localhost'
-default['mumble']['mysql']['port'] = '3306'
-default['mumble']['mysql']['prefix'] = ''
--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,1 +1,8 @@
+node.default['mumble']['mysql']['database'] = 'mumble'
+node.default['mumble']['mysql']['username'] = "mumble"
+node.set_unless['mumble']['mysql']['password'] = SecureRandom.base64(32).gsub(/\W/, "")
+node.default['mumble']['mysql']['host'] = 'localhost'
+node.default['mumble']['mysql']['port'] = '3306'
+node.default['mumble']['mysql']['prefix'] = ''
+
 include_recipe "mumble::default"
